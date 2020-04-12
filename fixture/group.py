99e7bb528007d8f2418f6140707c7a906465e2cd
agakax <agakax@gmail.com>
2020-04-12 09:03:22 +0200

    modify first group - adding a new method

--- fixture/group.py
+++ fixture/group.py
@@ -23,6 +23,26 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def modify_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # find first group and enter it
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("(//input[@name='edit'])[2]").click()
+        # edit group form
+        if group.name != "":
+            wd.find_element_by_name("group_name").clear()
+            wd.find_element_by_name("group_name").send_keys(group.name)
+        if group.header != "":
+            wd.find_element_by_name("group_header").clear()
+            wd.find_element_by_name("group_header").send_keys(group.header)
+        if group.footer != "":
+            wd.find_element_by_name("group_footer").clear()
+            wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
