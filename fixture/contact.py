3f51c0aca564068f2d6166cc6c3bb0cbb179ea3d
agakax <agakax@gmail.com>
2020-04-12 09:34:54 +0200

    modify first contact - adding new methods for modifying contacts on 2 ways

--- fixture/contact.py
+++ fixture/contact.py
@@ -63,10 +63,146 @@ class ContactHelper:
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
         self.return_to_home_page()
 
+    def modify_first_contact(self, contact):
+        wd = self.app.wd
+        self.app.open_home_page()
+        # select first contact and start editing it
+        wd.find_element_by_xpath("(//img[@title='Edit'])").click()
+        # edit contact form
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        if contact.photo_path != "":
+            wd.find_element_by_name("photo").send_keys(contact.photo_path)
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.telephone_home)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.telephone_mobile)
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.telephone_work)
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.telephone_fax)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact.email2)
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact.email3)
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birthday_day)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birthday_month)
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.birthday_year)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.anniversary_day)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.anniversary_month)
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.secondary_address)
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact.secondary_telephone_home)
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.secondary_notes)
+        # submit contact modification
+        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
+        self.return_to_home_page()
+
+    def modify_selected_fields_in_first_contact(self, contact):
+        wd = self.app.wd
+        self.app.open_home_page()
+        # select first contact and start editing it
+        wd.find_element_by_xpath("(//img[@title='Edit'])").click()
+        # edit contact form
+        if contact.first_name != "":
+            wd.find_element_by_name("firstname").clear()
+            wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        if contact.middle_name != "":
+            wd.find_element_by_name("middlename").clear()
+            wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+        if contact.last_name != "":
+            wd.find_element_by_name("lastname").clear()
+            wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        if contact.nickname != "":
+            wd.find_element_by_name("nickname").clear()
+            wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        if contact.photo_path != "":
+            wd.find_element_by_name("photo").send_keys(contact.photo_path)
+        if contact.title != "":
+            wd.find_element_by_name("title").clear()
+            wd.find_element_by_name("title").send_keys(contact.title)
+        if contact.company != "":
+            wd.find_element_by_name("company").clear()
+            wd.find_element_by_name("company").send_keys(contact.company)
+        if contact.address != "":
+            wd.find_element_by_name("address").clear()
+            wd.find_element_by_name("address").send_keys(contact.address)
+        if contact.telephone_home != "":
+            wd.find_element_by_name("home").clear()
+            wd.find_element_by_name("home").send_keys(contact.telephone_home)
+        if contact.telephone_mobile != "":
+            wd.find_element_by_name("mobile").clear()
+            wd.find_element_by_name("mobile").send_keys(contact.telephone_mobile)
+        if contact.telephone_work != "":
+            wd.find_element_by_name("work").clear()
+            wd.find_element_by_name("work").send_keys(contact.telephone_work)
+        if contact.telephone_fax != "":
+            wd.find_element_by_name("fax").clear()
+            wd.find_element_by_name("fax").send_keys(contact.telephone_fax)
+        if contact.email != "":
+            wd.find_element_by_name("email").clear()
+            wd.find_element_by_name("email").send_keys(contact.email)
+        if contact.email2 != "":
+            wd.find_element_by_name("email2").clear()
+            wd.find_element_by_name("email2").send_keys(contact.email2)
+        if contact.email3 != "":
+            wd.find_element_by_name("email3").clear()
+            wd.find_element_by_name("email3").send_keys(contact.email3)
+        if contact.homepage != "":
+            wd.find_element_by_name("homepage").clear()
+            wd.find_element_by_name("homepage").send_keys(contact.homepage)
+        if contact.birthday_day != "":
+            Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.birthday_day)
+        if contact.birthday_month != "":
+            Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.birthday_month)
+        if contact.birthday_year != "":
+            wd.find_element_by_name("byear").clear()
+            wd.find_element_by_name("byear").send_keys(contact.birthday_year)
+        if contact.anniversary_day != "":
+            Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.anniversary_day)
+        if contact.anniversary_month != "":
+            Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.anniversary_month)
+        if contact.anniversary_year != "":
+            wd.find_element_by_name("ayear").clear()
+            wd.find_element_by_name("ayear").send_keys(contact.anniversary_year)
+        if contact.address != "":
+            wd.find_element_by_name("address2").clear()
+            wd.find_element_by_name("address2").send_keys(contact.secondary_address)
+        if contact.secondary_telephone_home != "":
+            wd.find_element_by_name("phone2").clear()
+            wd.find_element_by_name("phone2").send_keys(contact.secondary_telephone_home)
+        if contact.secondary_notes != "":
+            wd.find_element_by_name("notes").clear()
+            wd.find_element_by_name("notes").send_keys(contact.secondary_notes)
+        # submit contact modification
+        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
+        self.return_to_home_page()
+
     def delete_first_contact(self):
         wd = self.app.wd
         self.app.open_home_page()
-        # select first group
+        # select first contact
         wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
---
+++ test/test_mod_contact.py
@@ -0,0 +1,34 @@
+from model.contact import Contact
+
+
+def test_modify_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(
+        first_name="Lev",
+        middle_name="Nikolayevich",
+        last_name="Tolstoy",
+        address=r"Yasnaya Polyana",
+        birthday_day="9",
+        birthday_month="September",
+        birthday_year="1828",
+        anniversary_day="20",
+        anniversary_month="November",
+        anniversary_year="1910",
+    ))
+    app.session.logout()
+
+
+def test_modify_selected_fields_in_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_selected_fields_in_first_contact(Contact(
+        first_name="Jan",
+        last_name="Brzechwa",
+        address=r"Kresy Wschodnie",
+        birthday_day="15",
+        birthday_month="August",
+        birthday_year="1898",
+        anniversary_day="2",
+        anniversary_month="July",
+        anniversary_year="1966",
+    ))
+    app.session.logout()
