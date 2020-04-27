447cd7631da3336983b6a7527e1bacd0639c5309
agakax <agakax@gmail.com>
2020-04-27 17:27:31 +0200

    upgrading tests for modificating first contact and group

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -5,7 +5,7 @@ def test_modify_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="test1"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(
+    contact = Contact(
         first_name="Lev",
         middle_name="Nikolayevich",
         last_name="Tolstoy",
@@ -16,10 +16,13 @@ def test_modify_first_contact(app):
         birthday_year="1828",
         anniversary_day="20",
         anniversary_month="November",
-        anniversary_year="1910",
-    ))
+        anniversary_year="1910")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
 def test_modify_first_contact_with_photo_deletion(app):
