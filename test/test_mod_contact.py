33ba3c47290ae04b7d8c5f4328c5f33ef5b5fe9b
agakax <agakax@gmail.com>
2020-04-27 15:05:00 +0200

    loading lists of contacts

--- test/test_mod_contact.py
+++ test/test_mod_contact.py
@@ -4,6 +4,7 @@ from model.contact import Contact
 def test_modify_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(first_name="test1"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(
         first_name="Lev",
         middle_name="Nikolayevich",
@@ -17,11 +18,14 @@ def test_modify_first_contact(app):
         anniversary_month="November",
         anniversary_year="1910",
     ))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
 
 def test_modify_first_contact_with_photo_deletion(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(nickname="test2"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(
         first_name="Jan",
         last_name="Brzechwa",
@@ -34,3 +38,5 @@ def test_modify_first_contact_with_photo_deletion(app):
         anniversary_month="July",
         anniversary_year="1966",
     ))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
