c8329a4f8cc858113d93ffadcedecf031f824fdf
agakax <agakax@gmail.com>
2020-05-17 23:02:14 +0200

    saving test data (for adding  groups) into an external json file

--- data/add_group.py
+++ data/add_group.py
@@ -1,6 +1,4 @@
 from model.group import Group
-import random
-import string
 
 
 constant = [Group(name="name1",
@@ -12,15 +10,3 @@ constant = [Group(name="name1",
             Group(name="name3",
                   header="header3",
                   footer="footer3")]
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-test_data = [Group()] + [
-    Group(name=random_string(prefix="name", maxlen=10), header=random_string(prefix="header", maxlen=20),
-          footer=random_string(prefix="footer", maxlen=15))
-    for i in range(3)
-]
---
+++ generator/group.py
@@ -0,0 +1,40 @@
+from model.group import Group
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 3
+f = "data/groups.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_data = [Group()] + [
+    Group(name=random_string(prefix="name", maxlen=10), header=random_string(prefix="header", maxlen=20),
+          footer=random_string(prefix="footer", maxlen=15))
+    for i in range(n)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
