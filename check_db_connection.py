3f2f8092b8fb3bb7fad2cfe5186c22e72450719b
agakax <agakax@gmail.com>
2020-05-25 21:34:09 +0200

    create new fixture for database and connect with db in it

--- check_db_connection.py
+++ check_db_connection.py
@@ -2,16 +2,18 @@ import pymysql.cursors
 import os.path
 import json
 
-
 config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "target.json")
 with open(config_file) as f:
     target = json.load(f)
 
-connection = pymysql.connect(host=target["db_host"], database=target["db_name"], user=target["db_username"], password=target["db_password"])
+connection = pymysql.connect(host=target["db_host"], database=target["db_name"], user=target["db_username"],
+                             password=target["db_password"])
 
 try:
     cursor = connection.cursor()
-    cursor.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s'" % (target["db_name"], "group_list"))
+    cursor.execute(
+        "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s'" % (
+            target["db_name"], "group_list"))
     column_names = []
     for row in cursor.fetchall():
         column_names.append(row[0])
