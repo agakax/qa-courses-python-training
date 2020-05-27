370c5871c79fb324d40858b3eee7c7e55633e8a2
agakax <agakax@gmail.com>
2020-05-27 14:35:36 +0200

    update method for checking connection

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,25 +1,19 @@
 import pymysql.cursors
 import os.path
 import json
+from fixture.db import DbFixture
 
 config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "target.json")
 with open(config_file) as f:
     target = json.load(f)
 
-connection = pymysql.connect(host=target["db_host"], database=target["db_name"], user=target["db_username"],
-                             password=target["db_password"])
+db = DbFixture(host=target["db"]["host"], name=target["db"]["name"], user=target["db"]["user"],
+               password=target["db"]["password"])
 
 try:
-    cursor = connection.cursor()
-    cursor.execute(
-        "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s'" % (
-            target["db_name"], "group_list"))
-    column_names = []
-    for row in cursor.fetchall():
-        column_names.append(row[0])
-    print(tuple(column_names))
-    cursor.execute("SELECT * FROM group_list")
-    for row in cursor.fetchall():
-        print(row)
+    groups = db.get_group_list()
+    for group in groups:
+        print(group)
+    print(len(groups))
 finally:
-    connection.close()
+    db.destroy()
