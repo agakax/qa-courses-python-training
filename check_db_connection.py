a065e515491fdf6409f08785bd57ef5ad13c6c63
agakax <agakax@gmail.com>
2020-05-27 21:24:56 +0200

    adding additional methods to ORM

--- check_db_connection.py
+++ check_db_connection.py
@@ -2,6 +2,7 @@ import pymysql.cursors
 import os.path
 import json
 from fixture.orm import ORMFixture
+from model.group import Group
 
 config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "target.json")
 with open(config_file) as f:
@@ -11,7 +12,7 @@ db = ORMFixture(host=target["db"]["host"], name=target["db"]["name"], user=targe
                 password=target["db"]["password"])
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id_group="270"))
     for item in l:
         print(item)
     print(len(l))
